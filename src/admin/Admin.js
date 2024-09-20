0ef3b508c4e006d11c30e720cbe61a10a9ca8ba7
sarthak <thesarthakchauhan@gmail.com>
2024-09-20 16:40:47 +0530

    “update”

--- src/admin/Admin.js
+++ src/admin/Admin.js
@@ -8,14 +8,17 @@ export default function Admin() {
   // If the user's role is not "admin", return null (renders nothing)
   if (profile.role !== 'admin') {
     return (
+
         <div className="flex justify-center items-center h-screen">
-        <h1 className="text-2xl font-bold text-red-500">you dont have access</h1>
-      </div>
+            <h1 className="text-2xl font-bold text-red-500">no admin access</h1>
+        </div>
     );
   }
 
   // If the user is an admin, render the admin content (e.g., admin button)
   return (
-<h1>ram ram admin</h1>
+    <div className="flex justify-center items-center h-screen">
+        <h1 className="text-2xl font-bold text-green-500">admin</h1>
+    </div>
   );
 }
